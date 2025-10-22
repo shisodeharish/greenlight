05181e76fd75174984d2dc94266423e43c74a54a
Harish Shisode <harishhshisode@gmail.com>
2025-10-22 14:44:42 +0530

    Creating additional activation tokens

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,11 +23,11 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
-	// Add the PUT /v1/users/password endpoint.
 	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	// Add the POST /v1/tokens/password-reset endpoint.
+	// Add the POST /v1/tokens/activation endpoint.
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
